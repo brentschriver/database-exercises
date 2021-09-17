1ec8963bce392cdda42cdd6c45e35fa0b36410b8
Brent <brent.schriver@gmail.com>
2021-09-17 14:11:38 -0500

    Updated queries from 'select' to 'describe'

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -5,19 +5,20 @@ use employees;
 show tables;
 
 -- Explore the employees table. What different data types are present on this table?
-select * from employees;
+describe employees;
 	-- int, date, string, char, varchar
 
 -- Which table(s) do you think contain a numeric type column?
-	-- departments, employees, salaries
-select * from salaries;
+	-- employees, salaries
+describe employees;
 
 -- Which table(s) do you think contain a string type column?
 	-- departments, dept_emp, dept_manager, employees, titles
+describe dept_emp;
 
 -- Which table(s) do you think contain a date?
 	-- dept_emp, employees, titles, salaries
-select * from salaries;
+describe salaries;
 
 -- What is the relationship between the employees and the departments tables?
 	-- Each employee should be linked with a department
